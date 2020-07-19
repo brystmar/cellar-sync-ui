ca8ec1f30f2002ad0809c2dad231e231742b0861
Thomas <tp.berg+github@gmail.com>
2020-07-18 21:57:10 -0500

    Added packages to support SASS

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter as Router} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import './css/global_sass_variables.css';
+import './css/global_sass_variables.css';
 import './css/autocomplete.css';
 import './css/grid-templates.css';
 import './css/list_items.css';
